8e4a7435785219278f543040e8a64ddb8ce088fa
Andrii <siryaka@gmail.com>
2017-11-28 18:31:38 +0200

    Added AuthenticationController and adorned its methods with corresponding attributes

---
+++ InstaBotPrototype/InstaBotPrototype/Controllers/AuthenticationController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace InstaBotPrototype.Controllers
+{
+    [Route("api/[controller]")]
+    public class AuthenticationController : Controller
+    {
+        [HttpGet]
+        int Login([FromBody]string login, [FromBody]string password)
+        {
+            return 0;
+        }
+
+        [HttpPost]
+        int Register([FromBody]string login, [FromBody]string password)
+        {
+            return 0;
+        }
+    }
+}
