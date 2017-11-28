ce71e14f4d3b9281565a663752084b4fa65274cb
Andrii <siryaka@gmail.com>
2017-11-28 21:38:22 +0200

    Added some comments

--- InstaBotPrototype/DBMigrator/Migrations/CreateMigrationsTable.cs
+++ InstaBotPrototype/DBMigrator/Migrations/CreateMigrationsTable.cs
@@ -2,13 +2,13 @@
 
 namespace DBMigrator
 {
-    [Indexer(0)]
-    class CreateMigrationsTable : DBMigration
+    [Indexer(0)] // Apply attribute (index must be greater than indexes used before)
+    class CreateMigrationsTable : DBMigration // Derive from base class
     {
-        public CreateMigrationsTable(object factory, object connection) : base(factory as DbProviderFactory, connection as DbConnection)
+        public CreateMigrationsTable(object factory, object connection) : base(factory as DbProviderFactory, connection as DbConnection) // Call base constructor
         {
-            ApplyCommand.CommandText = "create table dbo.Migrations (Name nvarchar(100), ApplyTime DateTime)";
-            ReverseCommand.CommandText = "drop table dbo.Migrations";
+            ApplyCommand.CommandText = "create table dbo.Migrations (Name nvarchar(100), ApplyTime DateTime)"; // Hardcode database apply ...
+            ReverseCommand.CommandText = "drop table dbo.Migrations"; // ... and reverse commands
         }
-    }
+    } // Well done!
 }
