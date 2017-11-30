d86e2dbd71c9ef6a92df3bfea9d46460fd1aedb3
Andrii <siryaka@gmail.com>
2017-11-30 17:04:00 +0200

    Added identity to users table

--- InstaBotPrototype/DBMigrator/Migrations/CreateUsersTable.cs
+++ InstaBotPrototype/DBMigrator/Migrations/CreateUsersTable.cs
@@ -7,7 +7,7 @@ namespace DBMigrator
     {
         public CreateUsersTable(object factory, object connection) : base(factory as DbProviderFactory, connection as DbConnection)
         {
-            ApplyCommand.CommandText = "create table dbo.Users (Id integer, Login nvarchar(100), Password nvarchar(100), primary key(Id))";
+            ApplyCommand.CommandText = "create table dbo.Users (Id int identity, Login nvarchar(100), Password nvarchar(100), primary key(Id))";
             ReverseCommand.CommandText = "drop table dbo.Users";
         }
     }
