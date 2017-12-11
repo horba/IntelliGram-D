813b4bc0f9daa2e2781483c3cb8719fe098bfdb3
Roman Shevchenko <groleore@gmail.com>
2017-12-11 14:37:38 +0300

    Initial commit Feature #17 AuthenticationController

---
+++ InstaBotPrototype/InstaBotPrototype/Services/IAuthenticationService.cs
@@ -0,0 +1,9 @@
+﻿using InstaBotPrototype.Models;
+namespace InstaBotPrototype.Services
+{
+    public interface IAuthenticationService
+    {
+        int Login(LoginModel model);
+        int Register(LoginModel model);
+    }
+}
