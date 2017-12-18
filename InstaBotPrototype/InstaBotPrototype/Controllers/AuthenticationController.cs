d546e8b569938f101a091b69104c1f9e61922480
Roman Shevchenko <groleore@gmail.com>
2017-12-18 23:27:42 +0300

    Change return values to json.Remove cookie setting.#17

--- InstaBotPrototype/InstaBotPrototype/Controllers/AuthenticationController.cs
+++ InstaBotPrototype/InstaBotPrototype/Controllers/AuthenticationController.cs
@@ -3,36 +3,30 @@ using Microsoft.AspNetCore.Mvc;
 using InstaBotPrototype.Services;
 namespace InstaBotPrototype.Controllers
 {
-    [Route("/[controller]/[action]")]
+    [Route("api/[controller]/[action]")]
     public class AuthenticationController : Controller
     {
         private readonly IAuthenticationService _authenticationService;
         public AuthenticationController (IAuthenticationService service) {
             _authenticationService = service;
         }        
-        [HttpPost]
+        [HttpGet]
         public IActionResult Login (LoginModel loginModel)
         {
             var loginResult = _authenticationService.Login(loginModel);
-            if (loginResult != -1) {
-                var cookieOptions = new Microsoft.AspNetCore.Http.CookieOptions
-                {
-                    Expires = System.DateTime.Now.AddDays(1)
-                };
-                Response.Cookies.Append("UserIdentifier",loginResult.ToString(),cookieOptions);
-                return Content("You have signed in successfully!");
-               
-            }
-            return Content("Wrong login or password!");
+            if (loginResult != -1)
+                return new ObjectResult(new {sessionID = loginResult});
+            else
+                return NotFound();
         }
         [HttpPost]
         public IActionResult Register(LoginModel loginModel)
         {
             var registerResult = _authenticationService.Register(loginModel);
             if (registerResult != -1)
-                return Content("You have signed up successfully! Now you can sign in using your login and password.");
+                return new ObjectResult(registerResult);
             else
-                return Content("Oooops! Something have gone wrong. Please, sign up again!");
+                return NotFound();
         }
     }
 }
