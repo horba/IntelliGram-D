84a61e7d808a11551b757a8e007ea2b4f24f2578
Groleore <groleore@gmail.com>
2017-12-31 12:50:13 +0300

    Add migration for creating Sessions table

---
+++ InstaBotPrototype/DBMigrator/Migrations/M201712311235_CreateTableSessions.cs
@@ -0,0 +1,25 @@
+﻿using System.Data.Common;
+
+namespace DBMigrator.Migrations
+{
+    [Indexer(201712311235)]
+    class M201712311235_CreateTableSessions : DBMigration
+    {
+        public M201712311235_CreateTableSessions(object factory, object connection) : base(factory as DbProviderFactory, connection as DbConnection)
+        {
+            ApplyCommand.CommandText =
+            @"create table dbo.Sessions 
+					(
+						Id int NOT NULL, 
+						SessionId uniqueidentifier NOT NULL, 
+						LoginTime DateTime DEFAULT CURRENT_TIMESTAMP, 
+						CONSTRAINT PK_Sessions PRIMARY KEY (Id,LoginTime), 
+						CONSTRAINT FK_Sessions FOREIGN KEY (Id)     
+						REFERENCES dbo.Users (Id)     
+						ON DELETE CASCADE    
+						ON UPDATE CASCADE
+			);";
+            ReverseCommand.CommandText = "drop table dbo.Sessions";
+        }
+    }
+}
