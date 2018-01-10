1f4a58bbed38b8ab69e30c10a5078940cf5d1703
vladkvts <vladyakovets@gmail.com>
2018-01-11 00:15:21 +0200

    Added migrations to the database. Created tables: Configuration,Topic,Tag,ConfigTopic,ConfigTag,TelegramUsers

---
+++ InstaBotPrototype/DBMigrator/Migrations/M2018001082226_CreateConfigTopicTable.cs
@@ -0,0 +1,14 @@
+﻿using System.Data.Common;
+
+namespace DBMigrator.Migrations
+{
+    [Indexer(2018001082226)]
+    class M2018001082226_CreateConfigTopicTable: DBMigration
+    {
+        public M2018001082226_CreateConfigTopicTable(object factory, object connection) : base(factory as DbProviderFactory, connection as DbConnection)
+        {
+            ApplyCommand.CommandText = "create table dbo.ConfigTopic(ConfigID int not null, TopicID int not null)";
+            ReverseCommand.CommandText = "drop table dbo.ConfigTopic";
+        }
+    }
+}
---
+++ InstaBotPrototype/DBMigrator/Migrations/M2018001082236_CreateTelegramUsersTable.cs
@@ -0,0 +1,14 @@
+﻿using System.Data.Common;
+
+namespace DBMigrator.Migrations
+{
+    [Indexer(2018001082236)]
+    class M2018001082236_CreateTelegramUsersTable: DBMigration
+    {
+        public M2018001082236_CreateTelegramUsersTable(object factory, object connection) : base(factory as DbProviderFactory, connection as DbConnection)
+        {
+            ApplyCommand.CommandText = "create table dbo.TelegramUsers(ChatID int not null, FirstName nvarchar(128) not null, LastName nvarchar(128) not null, VerificationKey int, UserID int)";
+            ReverseCommand.CommandText = "drop table dbo.TelegramUsers";
+        }
+    }
+}
