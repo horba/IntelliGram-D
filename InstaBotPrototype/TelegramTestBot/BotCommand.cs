2a923f3214547cd410cd8a8fab9e52a9e16d2bc7
AkulovBogdan <akulovbog@gmail.com>
2018-01-13 15:00:39 +0200

    Add BotCommand class

---
+++ InstaBotPrototype/TelegramTestBot/BotCommand.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TelegramTestBot
+{
+    class BotCommand
+    {
+        public string Command { get; set; }
+        public AccessModifier Access { get; set; }
+    }
+}
