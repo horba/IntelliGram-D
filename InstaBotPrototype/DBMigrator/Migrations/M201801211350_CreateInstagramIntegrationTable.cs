3cfc52d959dd453121d4c58773a2135717d5da3d
Groleore <groleore@gmail.com>
2018-01-21 14:02:46 +0300

    Add CreateInstagramIntegrationTable migration #46

---
+++ InstaBotPrototype/DBMigrator/Migrations/M201801211350_CreateInstagramIntegrationTable.cs
@@ -0,0 +1,25 @@
+﻿using System.Data.Common;
+namespace DBMigrator.Migrations
+{
+    [Indexer(201801211350)]
+    class M201801211350_CreateInstagramIntegrationTable : DBMigration
+    {
+        public M201801211350_CreateInstagramIntegrationTable(object factory, object connection) : base(factory as DbProviderFactory, connection as DbConnection)
+        {
+            ApplyCommand.CommandText =
+            @"CREATE TABLE dbo.InstagramIntegration 
+					(
+						UserId int, 
+						InstagramId bigint NOT NULL,
+                        Nickname nvarchar(50) NOT NULL,
+                        AccessToken nvarchar(100) NOT NULL,
+						CONSTRAINT PK_InstagramIntegration PRIMARY KEY (ChatID), 
+						CONSTRAINT FK_InstagramIntegration_Users FOREIGN KEY (UserId)     
+						REFERENCES dbo.Users (Id)     
+						ON DELETE CASCADE    
+						ON UPDATE CASCADE
+			);";
+            ReverseCommand.CommandText = "drop table dbo.InstagramIntegration";
+        }
+    }
+}
