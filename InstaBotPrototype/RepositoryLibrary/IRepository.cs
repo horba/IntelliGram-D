8e661b1fb9f30e327da4afaaabf0d215eeb8c532
Andrii <siryaka@gmail.com>
2018-01-24 21:59:30 +0200

    Created UsersRepository and UserModel

---
+++ InstaBotPrototype/RepositoryLibrary/IRepository.cs
@@ -0,0 +1,10 @@
+﻿namespace RepositoryLibrary
+{
+    interface IRepository<T> where T : new()
+    {
+        void Add(T model);
+        T Get(int id);
+        void Update(T model);
+        void Delete(int id);
+    }
+}
---
+++ InstaBotPrototype/RepositoryLibrary/UsersRepository.cs
@@ -0,0 +1,13 @@
+﻿using InstaBotPrototype.Models;
+using System;
+
+namespace RepositoryLibrary
+{
+    class UsersRepository : IRepository<UserModel>
+    {
+        public void Add(UserModel model) => throw new NotImplementedException();
+        public void Delete(int id) => throw new NotImplementedException();
+        public UserModel Get(int id) => throw new NotImplementedException();
+        public void Update(UserModel model) => throw new NotImplementedException();
+    }
+}
